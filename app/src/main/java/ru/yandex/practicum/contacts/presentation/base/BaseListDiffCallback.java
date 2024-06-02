c9a4307566e12a1f1d23ba6029ad595c2e57c133
il <igurylev@yandex.ru>
2024-06-02 12:53:07 +0300

    Рабочий вариант BaseListDiffCallback. Убрал закомментированный ненужный код и ненужные импорты.

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -4,21 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
-public class BaseListDiffCallback <T extends ListDiffInterface<BaseListDiffCallback>> extends DiffUtil.ItemCallback<> {
+public class BaseListDiffCallback <T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
 
     @Override
-    public <T> boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+    public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.theSameAs(newItem);
     }
 
     @Override
-    public <T> boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+    public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.equals(newItem);
     }
 
     @Nullable
     @Override
-    public <T> Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
         return newItem;
     }
 }
